e4833e94d68f5917f5fd297b43fd6456bdec6210
Tomáš Křen <tomkren@gmail.com>
2016-05-11 15:43:29 +0200

    (ring 5)

---
+++ src/cz/tomkren/fishtron/eva/EvaledPop.java
@@ -0,0 +1,19 @@
+package cz.tomkren.fishtron.eva;
+
+import cz.tomkren.utils.Weighted;
+
+import java.util.List;
+
+// TODO BasicEvaledPop, IndivEvaledPop a TogetherEvaledPop maj spoustu kódu stejně, opravit
+// todo: tzn asi místo interfacu požít třídu od který budou dědit, to podstatný teď dělaj v konstruktoru
+
+public interface EvaledPop<Indiv extends Weighted> {
+    boolean isTerminating();
+    Indiv getBestIndividual();
+    Distribution<Indiv> getIndividuals();
+    List<Indiv> getTerminators();
+
+    default List<Indiv> getIndivList() {
+        return getIndividuals().getList();
+    }
+}
